bc2ee8fbe44d05d1dbbadf0e8c4904c910e91865
rushal <rushalchaurasia2020@gmail.com>
2024-11-27 04:24:01 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-black': '#000000',
+        'brand-gray': '#333333'
+      }
+    },
+  },
+  plugins: [],
+}
